f10b43c114200b0407403e212ba400c724e2f732
satheesh <satheeshthamarai162@gmail.com>
2025-08-04 19:24:53 +0530

    changes in index.js file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from CI/CD Pipeline!\n');
+  res.end('Hello from CI/CD Pipeline,its satheesh here!\n');
 });
 
 server.listen(port, () => {
